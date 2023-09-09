eea41c94000007769575eee0c4735240d8662e1e
candagdelen0 <candagdelen0@gmail.com>
2023-09-09 17:45:46 +0300

    model | görünüm

--- todo/models.py
+++ todo/models.py
@@ -4,3 +4,6 @@ class ToDo(models.Model):
     title = models.CharField(max_length=100)
     complete = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
