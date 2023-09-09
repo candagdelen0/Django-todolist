f7fa3c4972e8ded837a729b596783c18463f6111
candagdelen0 <candagdelen0@gmail.com>
2023-09-09 17:45:13 +0300

    Model Tanımlama

--- todo/models.py
+++ todo/models.py
@@ -1,1 +1,6 @@
 from django.db import models
+
+class ToDo(models.Model):
+    title = models.CharField(max_length=100)
+    complete = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
