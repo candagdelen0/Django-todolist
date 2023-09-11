3a9645e406e014a32821118b792e58f5e520a1ff
candagdelen0 <candagdelen0@gmail.com>
2023-09-11 17:56:04 +0300

    complete | urls

--- todo/urls.py
+++ todo/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.Home, name='home'),
     path('delete/<int:id>', views.Delete, name='Delete'),
     path('uncomplete/<int:id>',views.InComplete, name='InComplete'),
-    
+    path('complete/<int:id>',views.Complete, name="Complete"),
 ]
